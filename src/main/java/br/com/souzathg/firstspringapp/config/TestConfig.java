0b625308ac69649045ec7babe9b84188183c8ad1
Thiago Costa de Souza <thgsouza@outlook.com>
2022-11-28 12:10:56 -0300

    Many-to-many association with Join Table

--- src/main/java/br/com/souzathg/firstspringapp/config/TestConfig.java
+++ src/main/java/br/com/souzathg/firstspringapp/config/TestConfig.java
@@ -35,12 +35,6 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-
-        Order o1 = new Order(Instant.parse("2022-11-27T17:00:00-03:00"), OrderStatus.PAID, u1);
-        Order o2 = new Order(Instant.parse("2022-11-27T16:00:00-03:00"), OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(Instant.parse("2022-11-27T18:00:00-03:00"), OrderStatus.WAITING_PAYMENT, u1);
 
         Category c1 = new Category("Electronics");
         Category c2 = new Category("Books");
@@ -52,9 +46,28 @@ public class TestConfig implements CommandLineRunner {
         Product p4 = new Product("PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product("Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-        userRepository.saveAll(Arrays.asList(u1, u2));
-        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        p1.getCategories().add(c2);
+        p2.getCategories().add(c1);
+        p2.getCategories().add(c3);
+        p3.getCategories().add(c3);
+        p4.getCategories().add(c3);
+        p5.getCategories().add(c2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        Order o1 = new Order(Instant.parse("2022-11-27T17:00:00-03:00"), OrderStatus.PAID, u1);
+        Order o2 = new Order(Instant.parse("2022-11-27T16:00:00-03:00"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(Instant.parse("2022-11-27T18:00:00-03:00"), OrderStatus.WAITING_PAYMENT, u1);
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+
     }
 }
